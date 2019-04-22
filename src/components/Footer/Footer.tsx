e06cb6e143ecd9af34864d8a90e8549b937d903e
aleishawalters <aleisha.walters@hotmail.com>
2019-04-22 14:29:18 +1200

    minor adjustments

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import './Footer.css'
 
-export default function Header() {
+export default function Footer() {
     return (
         <div className="footer">
             <h1 className='footerText'>Created by Aleisha Walters for MSA phase 1 2019 </h1>
